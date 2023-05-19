b0e871fe50a42870e79c6289f62398db9d225633
Aditya Raj <91009983+adi1719@users.noreply.github.com>
2023-05-20 01:28:35 +0530

    Completed the Code mergeList.cpp
    Code was incomplete in mergeList.cpp

--- Lecture049 Linked List Day6/mergeList.cpp
+++ Lecture049 Linked List Day6/mergeList.cpp
@@ -1,71 +1,64 @@
-/************************************************************
-
-    Following is the linked list node structure.
-    
-    template <typename T>
-    class Node {
-        public:
-        T data;
-        Node* next;
-
-        Node(T data) {
-            next = NULL;
-            this->data = data;
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    private: 
+    ListNode* solve(ListNode* first, ListNode* second)
+    {
+        if(first->next==NULL)
+        {
+            first->next= second;
+            return first;
         }
+        ListNode* curr1 = first;
+        ListNode* next1 = curr1->next;
+        ListNode* curr2 = second;
+        ListNode* next2 = curr2->next;
+        while(next1 != NULL && curr2!= NULL)
+        {
+            if ( (curr2->val >= curr1->val) && (curr2->val <= next1->val) )
+        {
+            next2 = curr2->next;
+            curr2->next = next1;
+            curr1->next = curr2;
 
-        ~Node() {
-            if (next != NULL) {
-                delete next;
-            }
-        }
-    };
-
-************************************************************/
-
-void solve(Node<int>* first, Node<int>* second) {
-    
-    
-    Node* curr1 = first;
-    Node* next1 = curr1 -> next;
-    
-    Node* curr2 = second;
-    Node* next2 = curr2 -> next;
-    
-    while(next1 != NULL && curr2 != NULL) {
-        
-        if( (curr2 -> data >= curr1 -> data ) 
-           && ( curr2 -> data <= next1 -> data)) {
-            
-            curr1 -> next = curr2;
-            curr2 -> next = next1;
             curr1 = curr2;
             curr2 = next2;
         }
-        else {
-            
+        else
+        {
+            //cuur1 and next1 will move forward
+            curr1 = next1;
+            next1 = next1->next;
+            if(next1==NULL)
+            {
+                curr1->next = curr2;
+                return first;
+            }
         }
-        
-        
-    }
-    
-    
-}
-
-Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
-{
-    if(first == NULL)
-        return second;
-    
-    if(second == NULL)
+        }
         return first;
-    
-    if(first -> data <= second -> data ){
-        solve(first, second);
     }
-    else
-    {
-        solve(second, first);
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1==NULL)
+        {
+            return list2;
+        }
+        if(list2==NULL)
+        {
+            return list1;
+        }
+        if(list1->val <= list2->val)
+            return solve(list1,list2);
+        else
+            return solve(list2, list1);
     }
-    
-    
-}
+};
