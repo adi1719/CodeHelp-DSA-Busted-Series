4de54fd101fd43b110a01888e96ebf8973bb08c7
Aditya Raj <91009983+adi1719@users.noreply.github.com>
2023-05-20 01:39:35 +0530

    Completed mergeList.cpp
    Code was incomplete

--- Lecture049 Linked List Day6/mergeList.cpp
+++ Lecture049 Linked List Day6/mergeList.cpp
@@ -1,3 +1,5 @@
+#include <bits/stdc++.h>
+
 /************************************************************
 
     Following is the linked list node structure.
@@ -21,51 +23,54 @@
     };
 
 ************************************************************/
+Node<int>* solve(Node<int> *first, Node<int> *second)
+    {
+        if (first->next == NULL)
+        {
+            first->next = second;
+            return first;
+        }
+        Node<int> *curr1 = first;
+        Node<int> *next1 = curr1->next;
+        Node<int> *curr2 = second;
+        Node<int> *next2 = curr2->next;
+        while (next1 != NULL && curr2 != NULL)
+        {
+            if ((curr2->data >= curr1->data) && (curr2->data <= next1->data))
+            {
+                next2 = curr2->next;
+                curr2->next = next1;
+                curr1->next = curr2;
 
-void solve(Node<int>* first, Node<int>* second) {
-    
-    
-    Node* curr1 = first;
-    Node* next1 = curr1 -> next;
-    
-    Node* curr2 = second;
-    Node* next2 = curr2 -> next;
-    
-    while(next1 != NULL && curr2 != NULL) {
-        
-        if( (curr2 -> data >= curr1 -> data ) 
-           && ( curr2 -> data <= next1 -> data)) {
-            
-            curr1 -> next = curr2;
-            curr2 -> next = next1;
-            curr1 = curr2;
-            curr2 = next2;
+                curr1 = curr2;
+                curr2 = next2;
+            }
+            else
+            {
+                // cuur1 and next1 will move forward
+                curr1 = next1;
+                next1 = next1->next;
+                if (next1 == NULL)
+                {
+                    curr1->next = curr2;
+                    return first;
+                }
+            }
         }
-        else {
-            
-        }
-        
-        
+        return first;
     }
-    
-    
-}
-
 Node<int>* sortTwoLists(Node<int>* first, Node<int>* second)
 {
-    if(first == NULL)
-        return second;
-    
-    if(second == NULL)
-        return first;
-    
-    if(first -> data <= second -> data ){
-        solve(first, second);
-    }
-    else
-    {
-        solve(second, first);
-    }
-    
-    
+   if (first == NULL)
+        {
+            return second;
+        }
+        if (second == NULL)
+        {
+            return first;
+        }
+        if (first->data <= second->data)
+            return solve(first, second);
+        else
+            return solve(second, first);
 }
